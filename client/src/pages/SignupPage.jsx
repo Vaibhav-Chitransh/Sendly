6eb13474ba63716b28358be9f176bffedc5e5c6b
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2025-03-14 19:20:48 +0530

    modified signup page

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -58,12 +58,12 @@ export function SignupPage() {
             </LabelInputContainer>
 
             <button
-              className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-md hover:shadow-lg transition-shadow cursor-pointer"
-              type="submit"
-            >
-              Sign up &rarr;
-              <BottomGradient />
-            </button>
+        className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-md hover:shadow-lg transition-shadow cursor-pointer"
+        type="submit"
+      >
+        Sign up &rarr;
+        <BottomGradient />
+      </button>
 
             <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
