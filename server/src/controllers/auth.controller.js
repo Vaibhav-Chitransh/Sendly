becf974f9b109f43372dcf21f74456acebf05aaa
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2025-03-30 00:12:08 +0530

    updated signup and login controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -3,12 +3,12 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res) => {
+    const {name, email, password} = req.body;
+    if(!name || !email || !password) return res.status(400).json({message: 'All fields are required'});
+
+    if(password.length < 6) return res.status(400).json({message: 'Password must be atleast 6 characters long'});
+
     try {
-        const {name, email, password} = req.body;
-
-        if(!name || !email || !password) return res.status(400).json({message: 'All fields are required'});
-        if(password.length < 6) return res.status(400).json({message: 'Password must be atleast 6 characters long'});
-
         const existedUser = await User.findOne({email});
         if(existedUser) return res.status(400).json({message: 'User already exists'});
 
@@ -17,6 +17,15 @@ export const signup = async (req, res) => {
 
         const newUser = await User.create({name, email, password: hash});
         await newUser.save();
+
+        const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET_KEY, {expiresIn: '7d'});
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
         return res.status(201).json({name: newUser.name, email: newUser.email});
     } catch (error) {
         console.log(error.message);
@@ -25,18 +34,23 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    try {
-        const {email, password} = req.body;
+    const {email, password} = req.body;
+    if(!email || !password) return res.status(400).json({message: 'All fields are required'});
 
-        if(!email || !password) return res.status(400).json({message: 'All fields are required'});
-
+    try {   
         const existedUser = await User.findOne({email});
         if(!existedUser) return res.status(400).json({message: 'Invalid credentials'});
 
         const isPwdCorrect = await bcrypt.compare(password, existedUser.password);
         if(!isPwdCorrect) return res.status(400).json({message: 'Invalid credentials'});
 
-        const token = jwt.sign({userId: existedUser._id}, process.env.JWT_SECRET_KEY, {expiresIn: '1d'});
+        const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET_KEY, {expiresIn: '7d'});
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
 
         return res.status(200).json({token});
     } catch (error) {
